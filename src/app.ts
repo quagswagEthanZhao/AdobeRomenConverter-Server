16c1d730f53bdba7da69bf403be2acac7e2c3ad7
quagswagEthanZhao <yixinzhaousu@gmail.com>
2025-01-26 01:21:31 -0700

    Add logging

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Application } from 'express';
 import helmet from 'helmet';
 import romanRouter from './routes/roman.routes';
+import morgan from 'morgan';
 
 const app : Application = express();
 
 // middlewares
 app.use(express.json());
 app.use(helmet());
+app.use(morgan('dev')); // log req to console
 
 // Route
 app.use('/romannumeral', romanRouter);
